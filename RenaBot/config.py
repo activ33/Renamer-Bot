76b848458658c6f25264baa56daa7c6533ab3ae2
active <66892517+activ33@users.noreply.github.com>
2023-09-21 12:12:17 +0530

    Update config.py

--- RenaBot/config.py
+++ RenaBot/config.py
@@ -1,8 +1,8 @@
 import os
 from telethon import TelegramClient
 
-api_id = os.environ.get('API_ID',"3459865")
-api_hash = os.environ.get('API_HASH',"815c6badc99076374aee0e11c1305edd")
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
 Bot_token = os.environ.get('TOKEN',"6404643240:AAFCTb03FBn34kMVqaagNyj9tkyjGW6G5P0")
 Admin= os.environ.get('ADMIN_ID',"689061386")
 client=TelegramClient('bot',api_id,api_hash).start(bot_token=Bot_token)
