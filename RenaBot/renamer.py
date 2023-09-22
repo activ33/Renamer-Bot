ffddcaaf1096c9cb381968453706a52b2007c24f
active <66892517+activ33@users.noreply.github.com>
2023-09-22 19:03:43 +0530

    Update renamer.py

--- RenaBot/renamer.py
+++ RenaBot/renamer.py
@@ -256,9 +256,9 @@ async def batchrenamer(event):
         except:
             download_ext=""
         if os.path.exists(f"Thumbs\\{event.peer_id.user_id}.png"):
-            await upload_with_progress_bar(client=C,edited=probar_edit,file_location=download, name=f"{number}{filename_later}{file_name}{download_ext}",thumbnail=f"Thumbs\\{event.peer_id.user_id}.png")
+            await upload_with_progress_bar(client=C,edited=probar_edit,file_location=download, name=f"{number}{filename_later}{file_name}{download_ext}")
         else:
-            await upload_with_progress_bar(client=C,edited=probar_edit,file_location=download, name=f"{file_name} {number}{filename_later}{download_ext}")
+            await upload_with_progress_bar(client=C,edited=probar_edit,file_location=download, name=f"{number}{filename_later}{file_name}{download_ext}")
         j=j+1
         await replyedit_message.edit(f"File {j}")
         number=number+1
